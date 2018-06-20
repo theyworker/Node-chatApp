0874217ec64e5ac059b38a996222f615e14edc07
theyworker <devaka.kumarasinghe@gmail.com>
2018-06-20 15:37:50 +0530

    routing done

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
 var path = require('path');
->>
 app.set('views', path.join(__dirname,'views'));
 app.engine('html', require('hogan-express'));
 app.set('view engine','html');
 app.use(express.static(path.join(__dirname, 'public')));
-app.route('/').get(function(req, res, next) {
-  // res.send('<h1>Hey Bro! </h1>');
-  res.render('index', {title:'Welcome to ChatCAT'})
-})
+// app.route('/').get(function(req, res, next) {
+//   // res.send('<h1>Hey Bro! </h1>');
+//   res.render('index', {title:'Welcome to ChatCAT'})
+// })
 
+require('./routes/routes.js') (express, app);
 app.listen(3000, function(){
   console.log("Chat App running on port 3000");
 })
---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+module.exports = function (express, app){
+  var router = express.Router();
+
+  router.get('/', function(req, res, next)
+  {
+    res.render('index',{'title':'Welcome to ChatCAT'});
+  })
+  router.get('/chatrooms', function(req, res, next)
+  {
+    res.render('chatrooms', {'title':'Chatrooms'});
+  })
+  app.use('/', router);
+}
